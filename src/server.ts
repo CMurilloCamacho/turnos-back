e5ec8dbb2aae72a7e4931dba2ff958d5500df32f
CMurilloCamacho <aldrin.edu.bo@gmail.com>
2025-04-15 11:01:58 -0400

    modificando url para despliegue

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,12 @@ import morgan from "morgan";
 import cors from "cors"
 import router from "./routes";
 const corsOptions = {
-    origin: 'https://vite-project-two-ecru.vercel.app/', // URL de tu frontend en Vercel
+    origin: 'https://vite-project-two-ecru.vercel.app', // URL de tu frontend en Vercel
   };
 
   
 
-  //https://vite-project-gncwez6vy-carlos-aldrins-projects.vercel.app/
+
 
 const server = express();
 server.use(express.json());
